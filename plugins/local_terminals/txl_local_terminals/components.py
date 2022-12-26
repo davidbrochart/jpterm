b9d9d497183bb82fa1ceff50e64f2a518e67a02c
David Brochart <david.brochart@gmail.com>
2022-12-26 11:01:33 +0100

    Create terminal plugin

--- plugins/local_terminals/txl_local_terminals/components.py
+++ plugins/local_terminals/txl_local_terminals/components.py
@@ -6,110 +6,47 @@ import shlex
 import struct
 import termios
 
-import pyte
 from asphalt.core import Component, Context
-from rich.console import RenderableType
-from rich.text import Text
-from textual.containers import Container
-from textual import events
 from textual.widget import Widget
 from textual.widgets._header import HeaderTitle
-from txl.base import Terminals, Header
+from txl.base import Terminals, TerminalFactory, Header
 from txl.hooks import register_component
 
 
-CTRL_KEYS = {
-    "left": "\u001b[D",
-    "right": "\u001b[C",
-    "up": "\u001b[A",
-    "down": "\u001b[B",
-}
 
-
-class PyteDisplay:
-    def __init__(self, lines):
-        self.lines = lines
-
-    def __rich_console__(self, console, options):
-        for line in self.lines:
-            yield line
-
-
-class TerminalsWidget(Widget, can_focus=True):
-    def __init__(self, send_queue, recv_queue, ncol, nrow):
-        self.recv_queue = recv_queue
-        self.send_queue = send_queue
-        self.nrow = nrow
-        self.ncol = ncol
-        self._display = PyteDisplay([Text()])
-        self._screen = pyte.Screen(ncol, nrow)
-        self.stream = pyte.Stream(self._screen)
-        asyncio.create_task(self.recv())
-        super().__init__()
-        self.focus()
-
-    def render(self) -> RenderableType:
-        return self._display
-
-    async def on_key(self, event: events.Key) -> None:
-        char = CTRL_KEYS.get(event.key) or event.character
-        await self.send_queue.put(["stdin", char])
-
-    async def recv(self):
-        while True:
-            message = await self.recv_queue.get()
-            cmd = message[0]
-            if cmd == "setup":
-                await self.send_queue.put(["set_size", self.nrow, self.ncol, 567, 573])
-            elif cmd == "stdout":
-                chars = message[1]
-                self.stream.feed(chars)
-                lines = []
-                for i, line in enumerate(self._screen.display):
-                    text = Text.from_ansi(line)
-                    x = self._screen.cursor.x
-                    if i == self._screen.cursor.y and x < len(text):
-                        cursor = text[x]
-                        cursor.stylize("reverse")
-                        new_text = text[:x]
-                        new_text.append(cursor)
-                        new_text.append(text[x + 1:])
-                        text = new_text
-                    lines.append(text)
-                self._display = PyteDisplay(lines)
-                self.refresh()
-
-
-class TerminalsMeta(type(Terminals), type(Container)):
+class TerminalsMeta(type(Terminals), type(Widget)):
     pass
 
 
-class LocalsTerminal(Terminals, Container, metaclass=TerminalsMeta):
+class LocalsTerminal(Terminals, Widget, metaclass=TerminalsMeta):
 
-    def __init__(self, header: Header):
+    def __init__(self, header: Header, terminal: TerminalFactory):
         self.header = header
-        self.ncol = 80
-        self.nrow = 24
-        self.data_or_disconnect = None
-        self.fd = self.open_terminal()
-        self.p_out = os.fdopen(self.fd, "w+b", 0)
-        self.event = asyncio.Event()
-        self.recv_queue = asyncio.Queue()
-        self.send_queue = asyncio.Queue()
+        self.terminal = terminal
+        self._send_queue = asyncio.Queue()
+        self._recv_queue = asyncio.Queue()
+        self._data_or_disconnect = None
+        self._event = asyncio.Event()
         super().__init__()
 
     async def open(self):
-        terminals_widget = TerminalsWidget(self.recv_queue, self.send_queue, self.ncol, self.nrow)
+        terminal = self.terminal(self._recv_queue, self._send_queue)
+        terminal.focus()
+        await self.mount(terminal)
+        terminal.set_size(self.size)
+        self._ncol = self.size.width
+        self._nrow = self.size.height
+        self._fd = self._open_terminal()
+        self._p_out = os.fdopen(self._fd, "w+b", 0)
         asyncio.create_task(self._run())
-        asyncio.create_task(self._send_data())
-        self.mount(terminals_widget)
+        asyncio.create_task(self._send())
         self.header.query_one(HeaderTitle).text = "Terminal"
 
-    def open_terminal(self):
+    def _open_terminal(self):
         pid, fd = pty.fork()
         if pid == 0:
             argv = shlex.split("bash")
-            env = dict(TERM="linux", LC_ALL="en_GB.UTF-8", COLUMNS=str(self.ncol), LINES=str(self.nrow))
+            env = dict(TERM="linux", LC_ALL="en_GB.UTF-8", COLUMNS=str(self._ncol), LINES=str(self._nrow))
             os.execvpe(argv[0], argv, env)
         return fd
 
@@ -118,31 +55,31 @@ class LocalsTerminal(Terminals, Container, metaclass=TerminalsMeta):
 
         def on_output():
             try:
-                self.data_or_disconnect = self.p_out.read(65536).decode()
-                self.event.set()
+                self._data_or_disconnect = self._p_out.read(65536).decode()
+                self._event.set()
             except Exception:
-                loop.remove_reader(self.p_out)
-                self.data_or_disconnect = None
-                self.event.set()
+                loop.remove_reader(self._p_out)
+                self._data_or_disconnect = None
+                self._event.set()
 
-        loop.add_reader(self.p_out, on_output)
-        await self.send_queue.put(["setup", {}])
+        loop.add_reader(self._p_out, on_output)
+        await self._send_queue.put(["setup", {}])
         while True:
-            msg = await self.recv_queue.get()
+            msg = await self._recv_queue.get()
             if msg[0] == "stdin":
-                self.p_out.write(msg[1].encode())
+                self._p_out.write(msg[1].encode())
             elif msg[0] == "set_size":
                 winsize = struct.pack("HH", msg[1], msg[2])
-                fcntl.ioctl(self.fd, termios.TIOCSWINSZ, winsize)
+                fcntl.ioctl(self._fd, termios.TIOCSWINSZ, winsize)
 
-    async def _send_data(self):
+    async def _send(self):
         while True:
-            await self.event.wait()
-            self.event.clear()
-            if self.data_or_disconnect is None:
-                await self.send_queue.put(["disconnect", 1])
+            await self._event.wait()
+            self._event.clear()
+            if self._data_or_disconnect is None:
+                await self._send_queue.put(["disconnect", 1])
             else:
-                await self.send_queue.put(["stdout", self.data_or_disconnect])
+                await self._send_queue.put(["stdout", self._data_or_disconnect])
 
 
 class LocalTerminalsComponent(Component):
@@ -152,8 +89,9 @@ class LocalTerminalsComponent(Component):
         ctx: Context,
     ) -> None:
         header = await ctx.request_resource(Header, "header")
+        terminal = await ctx.request_resource(TerminalFactory, "terminal")
         def terminals_factory():
-            return LocalsTerminal(header)
+            return LocalsTerminal(header, terminal)
         ctx.add_resource(terminals_factory, name="terminals", types=Terminals)
 
 c = register_component("terminals", LocalTerminalsComponent)
---
+++ plugins/terminal/txl_terminal/components.py
@@ -0,0 +1,92 @@
+import asyncio
+
+import pyte
+from asphalt.core import Component, Context
+from rich.console import RenderableType
+from rich.text import Text
+from textual import events
+from textual.widget import Widget
+from txl.base import TerminalFactory, Terminal
+from txl.hooks import register_component
+
+
+CTRL_KEYS = {
+    "left": "\u001b[D",
+    "right": "\u001b[C",
+    "up": "\u001b[A",
+    "down": "\u001b[B",
+}
+
+
+class PyteDisplay:
+    def __init__(self, lines):
+        self.lines = lines
+
+    def __rich_console__(self, console, options):
+        for line in self.lines:
+            yield line
+
+
+class TerminalMeta(type(Terminal), type(Widget)):
+    pass
+
+
+class _Terminal(Terminal, Widget, metaclass=TerminalMeta, can_focus=True):
+
+    def __init__(self, send_queue, recv_queue):
+        super().__init__()
+        self._send_queue = send_queue
+        self._recv_queue = recv_queue
+        self._display = PyteDisplay([Text()])
+        self._size_set = asyncio.Event()
+        asyncio.create_task(self._recv())
+
+    def set_size(self, size):
+        self._ncol = size.width
+        self._nrow = size.height
+        self._screen = pyte.Screen(self._ncol, self._nrow)
+        self._stream = pyte.Stream(self._screen)
+        self._size_set.set()
+
+    def render(self) -> RenderableType:
+        return self._display
+
+    async def on_key(self, event: events.Key) -> None:
+        char = CTRL_KEYS.get(event.key) or event.character
+        await self._send_queue.put(["stdin", char])
+
+    async def _recv(self):
+        await self._size_set.wait()
+        while True:
+            message = await self._recv_queue.get()
+            cmd = message[0]
+            if cmd == "setup":
+                await self._send_queue.put(["set_size", self._nrow, self._ncol, 567, 573])
+            elif cmd == "stdout":
+                chars = message[1]
+                self._stream.feed(chars)
+                lines = []
+                for i, line in enumerate(self._screen.display):
+                    text = Text.from_ansi(line)
+                    x = self._screen.cursor.x
+                    if i == self._screen.cursor.y and x < len(text):
+                        cursor = text[x]
+                        cursor.stylize("reverse")
+                        new_text = text[:x]
+                        new_text.append(cursor)
+                        new_text.append(text[x + 1:])
+                        text = new_text
+                    lines.append(text)
+                self._display = PyteDisplay(lines)
+                self.refresh()
+
+
+class TerminalComponent(Component):
+
+    async def start(
+        self,
+        ctx: Context,
+    ) -> None:
+        ctx.add_resource(_Terminal, name="terminal", types=TerminalFactory)
+
+c = register_component("terminal", TerminalComponent)
