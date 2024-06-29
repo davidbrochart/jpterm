440c84e7aaf7ceffc2b7d54c48a36e4d042d5cea
David Brochart <david.brochart@gmail.com>
2024-06-29 23:00:12 +0200

    Release v0.2.14 (#94)

---
+++ publish.py
@@ -0,0 +1,40 @@
+import subprocess
+from pathlib import Path
+
+import httpx
+import tomllib
+
+
+def run(cmd: str, cwd: str | None = None) -> list[str]:
+    res = subprocess.run(cmd.split(), capture_output=True, cwd=cwd)
+    return res.stdout.decode().splitlines()
+
+
+pyproject = tomllib.load(open("pyproject.toml", "rb"))
+for dependency in pyproject["project"]["dependencies"]:
+    idx = dependency.find("==")
+    version = dependency[idx + 2:].strip()
+    package = dependency[:idx].strip()
+    if package.startswith("txl"):
+        response = httpx.get(f"https://pypi.org/pypi/{package}/json")
+        releases = response.json()["releases"].keys()
+        if version not in releases:
+            print(f"Building {package}-{version}")
+            package_dir = Path()
+            dist_dir = "../dist"
+            if package != "txl":
+                package_dir /= "plugins"
+                dist_dir = f"../{dist_dir}"
+            package_dir /= package
+            run(f"hatch build {dist_dir}", cwd=str(package_dir))
+            for path in Path("dist").iterdir():
+                if f"{package}-{version}" in path.name:
+                    break
+            else:
+                raise RuntimeError(
+                    f"Wrong version for package {package}: did you forget to bump it to {version}?"
+                )
+
+
+print("Building jpterm")
+run("hatch build")
